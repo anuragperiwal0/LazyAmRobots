3ffa6668b1e338fe1856e704ac5caa905dfadcbd
anuragperiwal0 <116804267+anuragperiwal0@users.noreply.github.com>
2023-07-31 07:57:21 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AM-ROBOTS/LazyAmRobots /LazyAmRobots
+  git clone https://github.com/anuragperiwal0/LazyAmRobots /LazyAmRobots
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LazyAmRobots
